92b122a24203396ef0695b7c232aecd6d6e79c6a
Kingsley Ndonake <kndonake@gmail.com>
2026-01-09 13:06:42 +0000

    Clarify media file serving in urlpatterns for both DEBUG and production environments

--- paylio/urls.py
+++ paylio/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'paylio', 'static'))
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in both DEBUG and production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
